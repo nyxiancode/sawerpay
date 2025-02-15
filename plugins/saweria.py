4216a9de59e5fd8030dde6dfc14e9c67c786db0c
Nyxian Code <support@nyxiannetwork.web.id>
2025-02-15 16:46:30 +0700

    Update saweria.py

--- plugins/saweria.py
+++ plugins/saweria.py
@@ -1,6 +1,5 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from plugins.saweria import Saweria
 from database import Database
 
 db = Database()
