236bf87d47abe88452e7243d5c31e6a5474fbbde
Nyxian Code <support@nyxiannetwork.web.id>
2025-02-16 08:20:05 +0700

    Delete plugins directory

--- plugins/__init__.py
+++
@@ -1,5 +0,0 @@
-from .start import *
-from .talent import *
-from .saweria import *
-
-__all__ = ["start_handler", "talent_handlers", "saweria_handlers"]
--- plugins/start.py
+++
@@ -1,7 +0,0 @@
-#start.py
-from pyrogram import Client, filters
-from config import Config
-
-@Client.on_message(filters.command("start"))
-async def start_handler(client, message):
-    await message.reply("Halo! Selamat datang di bot Saweria.")
