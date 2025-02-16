989ef1a184b2564be87af7b8496ffd3a72db7ff4
Nyxian Code <support@nyxiannetwork.web.id>
2025-02-16 08:52:03 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
-import os
-
-class Config:
-    API_ID = int(os.getenv("API_ID", 123456))  # Ganti dengan API ID Anda
-    API_HASH = os.getenv("API_HASH", "your_api_hash")  # Ganti dengan API Hash Anda
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")  # Ganti dengan token bot Anda
-    OWNER_ID = int(os.getenv("OWNER_ID", 123456789))  # Ganti dengan ID owner
-    SAWERIA_EMAIL = os.getenv("SAWERIA_EMAIL", "your_email")  # Email Saweria
-    SAWERIA_PASSWORD = os.getenv("SAWERIA_PASSWORD", "your_password")  # Password Saweria
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")  # MongoDB URI
-    CHANNEL_DB = os.getenv("CHANNEL_DB", "your_channel_db")  # Channel untuk menyimpan log
+API_ID = 23795091
+API_HASH = "64a8dadb2a928e4a97e037e834d1f8db"
+BOT_TOKEN = "7928241298:AAEFqPzj8zuUhiOXeM91PgdT7QVfMoAOOTc"
+MONGO_URI = "mongodb+srv://yuna5:<password>@alterego.ltmv0bo.mongodb.net/?retryWrites=true&w=majority&appName=alterego"
+SAWERIA_EMAIL = "nyxiannetwork@gmail.com"
+SAWERIA_PASSWORD = "@Kita06_"
+OWNER_ID = 7494727691
+CHANNEL_DATABASE = "-1002184014745"
