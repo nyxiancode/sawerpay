23ef7fe4be151a752740eecdb381a86face04c09
Nyxian Code <support@nyxiannetwork.web.id>
2025-02-16 08:53:42 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 API_ID = 23795091
 API_HASH = "64a8dadb2a928e4a97e037e834d1f8db"
 BOT_TOKEN = "7928241298:AAEFqPzj8zuUhiOXeM91PgdT7QVfMoAOOTc"
-MONGO_URI = "mongodb+srv://yuna5:<password>@alterego.ltmv0bo.mongodb.net/?retryWrites=true&w=majority&appName=alterego"
+MONGO_URI = "mongodb+srv://yuna5:<password>@alterego.ltmv0bo.mongodb.net/saweria?retryWrites=true&w=majority&appName=alterego"
 SAWERIA_EMAIL = "nyxiannetwork@gmail.com"
 SAWERIA_PASSWORD = "@Kita06_"
 OWNER_ID = 7494727691
